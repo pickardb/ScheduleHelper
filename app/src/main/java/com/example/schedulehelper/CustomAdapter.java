12a9ec7a261eb374f3120052a1096993f09b0a5a
pickardb <blakepickard@hotmail.com>
2018-05-18 16:50:39 -0700

    added custom list view with indicators for monday-sunday

--- app/src/main/java/com/example/schedulehelper/CustomAdapter.java
+++ app/src/main/java/com/example/schedulehelper/CustomAdapter.java
@@ -1,13 +1,41 @@
 package com.example.schedulehelper;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
  * Created by Blake on 2018-05-18.
  */
 
-public class CustomAdapter {
+class CustomAdapter extends ArrayAdapter{
+
+     CustomAdapter( Context context, String[] NAMES) {
+        super(context, R.layout.availability_row, NAMES);
+    }
+
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater nameInfalter = LayoutInflater.from(getContext());
+        View customView = nameInfalter.inflate(R.layout.availability_row, parent, false);
+        String singleName = (String) getItem(position);
+        TextView nameText = (TextView) customView.findViewById(R.id.name_text);
+        ImageView sundayImage = (ImageView) customView.findViewById(R.id.sunday_image);
+        ImageView mondayImage = (ImageView) customView.findViewById(R.id.monday_image);
+        ImageView tuesdayImage = (ImageView) customView.findViewById(R.id.tuesday_image);
+        ImageView wednesdayImage = (ImageView) customView.findViewById(R.id.wednesday_image);
+        ImageView thursdayImage = (ImageView) customView.findViewById(R.id.thursday_image);
+        ImageView fridayImage = (ImageView) customView.findViewById(R.id.friday_image);
+        ImageView saturdayImage = (ImageView) customView.findViewById(R.id.saturday_image);
+
+        nameText.setText(singleName);
+        return customView;
+    }
 }
--- app/src/main/java/com/example/schedulehelper/MainActivity.java
+++ app/src/main/java/com/example/schedulehelper/MainActivity.java
@@ -3,10 +3,8 @@ package com.example.schedulehelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -25,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String[] NAMES = {"Test One", "Test Two", "Test Three"};
-        ListAdapter nameAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,NAMES);
+        ListAdapter nameAdapter = new CustomAdapter(this, NAMES);
         ListView nameList = (ListView) findViewById(R.id.nameList);
         nameList.setAdapter(nameAdapter);
 
